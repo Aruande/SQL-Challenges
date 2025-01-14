490134f7edb87e42316172da2118d086a3837a96
Marrio Hinkle <39510034+Aruande@users.noreply.github.com>
2025-01-13 19:16:09 -0600

    added description and author details

--- Basics/CategorizeTracksbyPrice.sql
+++ Basics/CategorizeTracksbyPrice.sql
@@ -1,3 +1,20 @@
+/*
+Authored By: Marrio Hinkle
+Create Date: 01/13/2025
+Description: Red30 Tech is conducting a comprehensive analysis of their digital music store's inventory to understand
+             the pricing distribution of tracks.
+              
+             write a SQL query that selects track names, composers, and unit prices, and categorizes each track based on its price.
+*/
+
+
+
+
+
+
+
+
+
 SELECT 
     Name AS "Track Name",
     Composer,
@@ -8,7 +25,7 @@ SELECT
         WHEN UnitPrice Between 1.00 AND 1.49 THEN 'Regular'
         WHEN UnitPrice Between 1.50 and 1.99 THEN 'Premium'
         Else 'Exclusive'
-    End As PriceCategory
+    End As PriceCategory  --add new column to result set table for categories 
 FROM 
     Track AS t
 ORDER BY
